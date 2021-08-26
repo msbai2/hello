99fe30a6d240879aa0113b78afb3d0b0c925f202
Mohammed Sbai <msbai2@huskers.unl.edu>
2021-08-26 21:30:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Mohammed Sbai
+ *         msbai2@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple my name program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mohammed Sbai!\n");
+
+  return 0;
+}
