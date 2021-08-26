41b44dd9f1dbacc7090cf1be0f88c1a493ae1a28
arrow9484 <akhandelwal2@huskers.unl.edu>
2021-08-26 23:02:41 +0000

    added my (Arnav) name and major

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /**
- * Author: Mohammed Sbai
- *         msbai2@huskers.unl.edu
+ * Author: Mohammed Sbai, Arnav Khandelwal
+ *         msbai2@huskers.unl.edu, akhandelwal2@huskers.unl.edu
  * Date: 2021/08/24
  *
  * A simple my name program in C
@@ -12,8 +12,9 @@
 int main(int argc, char **argv) {
 
   printf("Mohammed Sbai!!!\n");
-
+  printf("Arnav Khandelwal!\n");
   printf("Mechanical Engineering\n");
+  printf("Computer Science\n");
 
   return 0;
 }
